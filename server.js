1b92b41c8158531aab6ccc6af752583d26a8129a
visionNmission <balrochag@gmail.com>
2017-08-31 14:38:46 +0900

    로깅과 인증

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require(`express`)
+const morgan = require(`morgan`)
+var basicAuth = require('express-basic-auth')
+
 const app = express()
+
+app.use(basicAuth({
+  users: { 'admin': 'admin' },
+  challenge: true,
+  realm: 'Imb4T3st4pp'
+}))
+
+app.set(`view engine`, `ejs`)
+app.use(`/static`, express.static(`public`))
+app.use(morgan(`tiny`))
 
 app.get(`/`, (req, res) => {
   res.send(`Hello Node.js`)
 })
 
+app.listen(3000, () => {
+  console.log(`listening...`)
+})
